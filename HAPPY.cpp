a3b99df05f5be0f071843fcbdb1cf546e0abd3da
Psertded <110461934+Psertded@users.noreply.github.com>
2023-07-07 14:30:57 +0800

    Update HAPPY.cpp to 2.0!

--- HAPPY.cpp
+++ HAPPY.cpp
@@ -1,4 +1,4 @@
-﻿
+
 #define _CRT_SECURE_NO_WARNINGS
 
 
@@ -11,10 +11,22 @@
 
 using namespace std;
 
+void setr()
+{
+    srand(time(NULL) + clock() + rand());
+}
+
 int main()
 {
-
     HWND win = GetForegroundWindow();
+    HWND hwnd = win;
+    LONG style = GetWindowLong(hwnd, GWL_STYLE);
+    style = style & ~WS_CAPTION & ~WS_SIZEBOX;
+    SetWindowLong(hwnd, GWL_STYLE, style);
+    int screenX = GetSystemMetrics(SM_CXSCREEN);
+    int screenY = GetSystemMetrics(SM_CYSCREEN);
+    SetWindowPos(hwnd, NULL, 0, 0, screenX, screenY, SWP_NOZORDER);
+    SetWindowLong(win, GWL_STYLE, style);
     RECT rect;
     string text[17] =
     {
@@ -36,7 +48,7 @@ int main()
         "              2BB                 QBL   :bBBBQ:   uBBBBQUr.                                                                                           ",
         "              SBB                 BBJ :BBBP:         :PBBB:                                                                                           ",
     };
-    int x,b,c=180,d=37;
+    int x, b, c = 180, d = 37;
     string cmd;
     POINT m;
     system("mode 180,37");
@@ -53,98 +65,113 @@ int main()
     IMediaEvent* pEvent;
     CoInitialize(NULL);
     CoCreateInstance(CLSID_FilterGraph, NULL,
-    CLSCTX_INPROC, IID_IGraphBuilder, (void**)&pGBuilder);
+        CLSCTX_INPROC, IID_IGraphBuilder, (void**)&pGBuilder);
     pGBuilder->QueryInterface(IID_IMediaControl, (void**)&pMControl);
     pGBuilder->QueryInterface(IID_IMediaPosition, (void**)&pMPos);
     pGBuilder->RenderFile(L"backgroundmusic.mp3", NULL);
     pMPos->put_CurrentPosition(0);
     pMControl->Run();
     ios::sync_with_stdio(false);
-   for(int i=0;true;++i)
-   {
-       b = rand() % 8;
+    for (int i = 0; true; i=rand()%256)
+    {
+        b = rand() % 8;
 
-       GetCursorPos(&m);
-       switch (b)
-       {
-       case 0:
-           SetCursorPos(m.x + rand() % 15, m.y + rand() % 15);
-           c++; d++;
-           rect.left += 10;
-           rect.top += 10;
-           //left,top,right,top
-           MoveWindow(win, rect.left, rect.top, rect.right, rect.bottom, TRUE);
-           break;
-       case 1:
-           SetCursorPos(m.x - rand() % 15, m.y - rand() % 15);
-           c--; d--;
-           rect.left -= 10;
-           rect.top -= 10;
-           //left,top,right,top
-           MoveWindow(win, rect.left, rect.top, rect.right, rect.bottom, TRUE);
-           break;
-       case 2:
-           SetCursorPos(m.x + rand() % 15, m.y - rand() % 15);
-           c++, d--;
-           rect.left += 10;
-           rect.top -= 10;
-           //left,top,right,top
-           MoveWindow(win, rect.left, rect.top, rect.right, rect.bottom, TRUE);
-           break;
-       case 3:
-           SetCursorPos(m.x - rand() % 15, m.y + rand() % 15);
-           rect.left -= 10;
-           rect.top += 10;
-           //left,top,right,top
-           MoveWindow(win, rect.left, rect.top, rect.right, rect.bottom, TRUE);
-           c--, d++;
-           break;
-       }
-       cmd.clear();
-       cmd = "mode ";
-       cmd.append(to_string(c));
-       cmd.push_back(',');
-       cmd.append(to_string(d));
-       system(cmd.c_str());
-       system("cls");
-       x = rand()%5;
-       if (i > 257)
-       {
-           i = 0;
-       }
-       srand(rand());
-       SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), i);
-       cout << string(x, '\n');
-       for (int s = 0; s < 17; s++)
-       {
-           cout << string(b, ' ') << text[s]<<endl;
-       }
-       if ((GetAsyncKeyState((unsigned short)VK_ESCAPE) & 0x8000) ? 1 : 0)
-       {
-           exit(0);
-       }
-       if ((GetAsyncKeyState((unsigned short)'R') & 0x8000) ? 1 : 0)
-       {
-           c = 180;
-           d = 37;
-       }
-       if ((GetAsyncKeyState((unsigned short)'P') & 0x8000) ? 1 : 0)
-       {
-           MoveWindow
-           (
-               win, GetSystemMetrics(SM_CXFULLSCREEN) / 2 / 2, GetSystemMetrics(SM_CYFULLSCREEN) / 2 / 2,
-               GetSystemMetrics(SM_CXFULLSCREEN) / 2 + GetSystemMetrics(SM_CXFULLSCREEN) / 2 / 2,
-               GetSystemMetrics(SM_CYFULLSCREEN) / 2 + GetSystemMetrics(SM_CYFULLSCREEN) / 2 / 2,
-               TRUE
-           );
-           cmd.clear();
-           cmd = "mode ";
-           cmd.append(to_string(c));
-           cmd.push_back(',');
-           cmd.append(to_string(d));
-           system(cmd.c_str());
-           GetWindowRect(win, &rect);
-       }
+        GetCursorPos(&m);
+        switch (b)
+        {
+        case 0:
+
+            setr();
+            SetCursorPos(m.x + rand() % 15, m.y + rand() % 15);
+            c++; d++;
+            rect.left += 10;
+            rect.top += 10;
+            MoveWindow(win, rect.left, rect.top, rect.right, rect.bottom, TRUE);
+            break;
+        case 1:
+            setr();
+            SetCursorPos(m.x - rand() % 15, m.y - rand() % 15);
+            c--; d--;
+            rect.left -= 10;
+            rect.top -= 10;
+            MoveWindow(win, rect.left, rect.top, rect.right, rect.bottom, TRUE);
+            break;
+        case 2:
+            setr();
+            SetCursorPos(m.x + rand() % 15, m.y - rand() % 15);
+            c++, d--;
+            rect.left += 10;
+            rect.top -= 10;
+            MoveWindow(win, rect.left, rect.top, rect.right, rect.bottom, TRUE);
+            break;
+        case 3:
+            setr();
+            SetCursorPos(m.x - rand() % 15, m.y + rand() % 15);
+            rect.left -= 10;
+            rect.top += 10;
+            MoveWindow(win, rect.left, rect.top, rect.right, rect.bottom, TRUE);
+            c--, d++;
+            break;
+        }
+        cmd.clear();
+        cmd = "mode ";
+        cmd.append(to_string(c));
+        cmd.push_back(',');
+        cmd.append(to_string(d));
+        system(cmd.c_str());
+        system("cls");
+        x = rand() % 5;
+        if (i > 257)
+        {
+            i = 0;
+        }
+        srand(rand());
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), i);
+        cout << string(x, '\n');
+        for (int s = 0; s < 17; s++)
+        {
+            cout << string(b, ' ') << text[s] << endl;
+        }
+        cout << string(rand() % 10, '\n');
+        if ((GetAsyncKeyState((unsigned short)VK_ESCAPE) & 0x8000) ? 1 : 0)
+        {
+            exit(0);
+        }
+        if ((GetAsyncKeyState((unsigned short)'R') & 0x8000) ? 1 : 0)
+        {
+            style = GetWindowLong(hwnd, GWL_STYLE);
+            style = style & ~WS_CAPTION & ~WS_SIZEBOX;
+            SetWindowLong(hwnd, GWL_STYLE, style);
+            int screenX = GetSystemMetrics(SM_CXSCREEN);
+            int screenY = GetSystemMetrics(SM_CYSCREEN);
+            SetWindowPos(hwnd, NULL, 0, 0, screenX, screenY, SWP_NOZORDER);
+            SetWindowLong(win, GWL_STYLE, style);
+            GetWindowRect(win, &rect);
+        }
+        if ((GetAsyncKeyState((unsigned short)'P') & 0x8000) ? 1 : 0)
+        {
+            MoveWindow
+            (
+                win, GetSystemMetrics(SM_CXFULLSCREEN), GetSystemMetrics(SM_CYFULLSCREEN),
+                GetSystemMetrics(SM_CXFULLSCREEN) + GetSystemMetrics(SM_CXFULLSCREEN),
+                GetSystemMetrics(SM_CYFULLSCREEN) + GetSystemMetrics(SM_CYFULLSCREEN),
+                TRUE
+            );
+            style = GetWindowLong(hwnd, GWL_STYLE);
+            style = style & ~WS_CAPTION & ~WS_SIZEBOX;
+            SetWindowLong(hwnd, GWL_STYLE, style);
+            int screenX = GetSystemMetrics(SM_CXSCREEN);
+            int screenY = GetSystemMetrics(SM_CYSCREEN);
+            SetWindowPos(hwnd, NULL, 0, 0, screenX, screenY, SWP_NOZORDER);
+            SetWindowLong(win, GWL_STYLE, style);
+            GetWindowRect(win, &rect);
+            cmd.clear();
+            cmd = "mode ";
+            cmd.append(to_string(c));
+            cmd.push_back(',');
+            cmd.append(to_string(d));
+            system(cmd.c_str());
+            GetWindowRect(win, &rect);
+        }
     }
 }
-//2.0 TOMARROW!
